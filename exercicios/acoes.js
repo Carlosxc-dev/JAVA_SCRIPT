e01a8c63f035be439afc0ec9400220b4518c1689
carlos <chcruz64@gmail.com>
2021-11-08 13:26:30 -0300

    08/11

---
+++ exercicios/acoes.js
@@ -0,0 +1,19 @@
+function acao1() {
+    var resp = window.document.getElementById('saida')  // var resp e local a funcao acao1
+    resp.innerHTML += '<p>clicou no primeiro botao'     // mas pode ser global se no inicio
+}
+
+function acao2() {
+    var resp = window.document.getElementById('saida') // operacao += funciona como concatenacao 
+    resp.innerHTML += '<p>clicou no segndundo botao'    // faz os textos aparecerem sem apagar os
+}                                                       // ja criados
+
+function acao3() {
+    var resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>clicou no terceiro botao'
+}
+
+function acao4() {
+    var resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>clicou no quarto botao'
+}

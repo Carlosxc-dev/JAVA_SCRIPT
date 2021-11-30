39ee9a7cb226b60e79d6323a89c200ae38db11e5
carlos <chcruz64@gmail.com>
2021-11-29 21:08:38 -0300

    29/11

---
+++ curso_web_moderno/fundamentos/resp_atividades_fundamentos.js
@@ -0,0 +1,84 @@
+// 1
+function comprimentar(nome) {
+    return "ola " + nome + " !";
+}
+console.log(comprimentar("Maria"));
+
+// 2
+function converteIdadeEmAnosParaDias(idade) {
+    return idade * 365;
+}
+console.log("idade em dias= ", converteIdadeEmAnosParaDias(25));
+
+//3
+function calculaSalario(QuantHorasMes, valorHora) {
+    return "salario= " + QuantHorasMes * valorHora;
+}
+console.log(calculaSalario(150, 40.5));
+
+//4
+function retornaMes(numMes) {
+    const mes = ["janeiro", "fevereiro", "marco", "abril", "maio", "junho"];
+    return mes[numMes - 1];
+}
+console.log("mes= ", retornaMes(3));
+
+//5
+function maiorOuIgual(a, b) {
+    if (a >= b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log("valores sao maior ou iguais = ", maiorOuIgual(5, 10));
+
+//6 
+function inverso(valor) {
+
+    if (typeof (valor) == "string") {
+        return `esperado bool ou number, mas o tipo e ${typeof (valor)}`;
+    }
+    if (typeof (valor) == "number") {
+        return - valor;
+    }
+    if (typeof (valor) == "boolean") {
+        if (valor == true) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+console.log(inverso("casa"));
+
+
+//7
+function multiplicar(a = 0, b = 0) {
+    var conta = 0;
+    for (x = 0; x < b; x++) {
+        conta += a;
+    }
+    return (conta);
+}
+console.log("resultado da conta = ", multiplicar(5, 10));
+
+//8
+function simboloMais(valor) {
+    var simb = "";
+    for (x = 0; x < valor; x++) {
+        simb += "+"
+    }
+    return simb;
+}
+console.log(simboloMais(10));
+
+//9
+function receberPrimeiroUltimoElemto(array) {
+    var tam = array.length - 1;
+    return `primeiro elemento = ${array[0]} e segundo elem = ${array[tam]}`
+}
+console.log(receberPrimeiroUltimoElemto([1, 2, "ult"]));
+
+//10
